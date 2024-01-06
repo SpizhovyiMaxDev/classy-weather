ff65baa5d20265ee269236fa87b4dd6887727629
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2024-01-05 19:40:25 -0800

    Introduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.increment = this.increment.bind(this); 
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
+
+  decrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  render() {
+    const date = new Date("June 21, 2027");
+    date.setDate(date.getDate() + this.state.count);
+
+    return (
+      <div>
+        <button onClick={this.increment}>+</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
